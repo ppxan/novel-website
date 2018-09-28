40e370084fff207634bef2d4804de1d33644a2d8
pipi <872003725@qq.com>
2018-09-28 11:10:03 +0800

    create a new project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return(
+    <div>爱你哟</div>
+  );
+}
+}
+ReactDOM.render(<App />, document.getElementById('app'));
